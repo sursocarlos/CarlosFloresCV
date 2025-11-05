2e4d16b677c628dea09daa8528dc5c2d1c60e471
CarlosFlores <sursocarlos@gmail.com>
2025-11-05 15:24:11 +0100

    Cambios en Sobre_mi

--- src/components/Sobre_mi.jsx
+++ src/components/Sobre_mi.jsx
@@ -18,21 +18,16 @@ function Sobre_mi() {
           <div class="about-text">
             <h2>Sobre mí</h2>
             <p>
-              Soy Alex, un apasionado desarrollador de software con enfoque en
-              full stack. Actualmente estudio Desarrollo de Aplicaciones Web
-              (DAW).
+              Texto
             </p>
             <p>
-              Formación: Grado en Informática, cursos en Udemy sobre JavaScript
-              y Python.
+              Texto
             </p>
             <p>
-              Intereses: IA, desarrollo web responsive, y programación
-              competitiva.
+              Texto
             </p>
             <p>
-              Tecnologías favoritas: JavaScript, React (aunque aquí usamos
-              puro), Node.js.
+              Texto
             </p>
           </div>
         </div>
