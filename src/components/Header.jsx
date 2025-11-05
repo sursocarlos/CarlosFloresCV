dd3e0302ab7c9d860a9d0b74ef9446b673ca2a14
CarlosFlores <sursocarlos@gmail.com>
2025-11-05 15:17:28 +0100

    Cambios en el header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,45 +1,48 @@
 import React from "react";
 import "../styles/Header.css";
+import logo from "../assets/logo3.png";
 
 function Header() {
   return (
-    <div className="header-wrapper"> 
-    <header className="header">
-      <h1>Carlos Flores</h1>
-      <nav>
-        <a href="#inicio">Inicio</a>
-        <a href="#sobre-mi">Sobre mí</a>
-        <a href="#experiencia">Habilidades</a>
-        <a href="#certificaciones">Certificaciones</a>
-        <a href="#contacto">Contacto</a>
-      </nav>
-
-      <div className="body">
-        <div className="tabs">
-          <input
-            defaultChecked
-            defaultValue="HTML"
-            name="fav_language"
-            id="html"
-            type="radio"
-            className="input"
-          />
-          <label htmlFor="html" className="label">
-            Español
-          </label>
-          <input
-            defaultValue="JavaScript"
-            name="fav_language"
-            id="javascript"
-            type="radio"
-            className="input"
-          />
-          <label htmlFor="javascript" className="label">
-            Ingles
-          </label>
+    <div className="header-wrapper">
+      <header className="header">
+        <div className="logo">
+          <img src={logo} alt="aa" />
         </div>
-      </div>
-    </header>
+        <nav>
+          <a href="#inicio">Inicio</a>
+          <a href="#sobre-mi">Sobre mí</a>
+          <a href="#experiencia">Habilidades</a>
+          <a href="#certificaciones">Certificaciones</a>
+          <a href="#contacto">Contacto</a>
+        </nav>
+
+        <div className="body">
+          <div className="tabs">
+            <input
+              defaultChecked
+              defaultValue="HTML"
+              name="fav_language"
+              id="html"
+              type="radio"
+              className="input"
+            />
+            <label htmlFor="html" className="label">
+              Español
+            </label>
+            <input
+              defaultValue="JavaScript"
+              name="fav_language"
+              id="javascript"
+              type="radio"
+              className="input"
+            />
+            <label htmlFor="javascript" className="label">
+              Ingles
+            </label>
+          </div>
+        </div>
+      </header>
     </div>
   );
 }
