07252470cfe60cfd30d9b3be151de83bd58bdf46
CarlosFlores <sursocarlos@gmail.com>
2025-11-10 20:47:52 +0100

    Mejora de resposive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "../styles/Header.css";
 import logo from "../assets/logo3.png";
 import { LanguageContext } from "../context/LanguageContext";
@@ -6,22 +6,66 @@ import { LanguageContext } from "../context/LanguageContext";
 function Header() {
   const { lang, toggleLang } = useContext(LanguageContext);
 
+  // Pruebas cambios random (Eliminar)
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => setMenuOpen(!menuOpen);
+
+  const handleLinkClick = () => setMenuOpen(false);
+  // Pruebas cambios random (Eliminar)
+
   return (
     <div className="header-wrapper">
       <header className="header">
         <div className="logo">
           <img src={logo} alt="logo" />
         </div>
-        
+
+        {/* Pruebas cambios random (Eliminar)*/}
+        <button
+          className={`menu-toggle ${menuOpen ? "open" : ""}`}
+          onClick={handleMenuToggle}
+          aria-label="Abrir menú"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        {/* Pruebas cambios random (Eliminar) */}
+
+        {/*  CODIGO ORIGINAL (NO ELIMINAR)
         <nav>
           <a href="#sobre-mi">{lang === "es" ? "Inicio" : "Home"}</a>
-          <a href="#experiencia">{lang === "es" ? "Experiencia" : "Experience"}</a>
+          <a href="#experiencia">
+            {lang === "es" ? "Experiencia" : "Experience"}
+          </a>
           <a href="#habilidades">{lang === "es" ? "Habilidades" : "Skills"}</a>
           <a href="#certificaciones">
             {lang === "es" ? "Certificaciones" : "Certifications"}
           </a>
           <a href="#contacto">{lang === "es" ? "Contacto" : "Contact"}</a>
         </nav>
+        CODIGO ORIGINAL (NO ELIMINAR) */}
+
+        {/* Pruebas cambios random (Eliminar) */}
+        <nav className={`nav ${menuOpen ? "active" : ""}`}>
+            <a href="#sobre-mi" onClick={handleLinkClick}>
+              {lang === "es" ? "Sobre mí" : "About Me"}
+            </a>
+            <a href="#experiencia" onClick={handleLinkClick}>
+              {lang === "es" ? "Experiencia" : "Experience"}
+            </a>
+            <a href="#habilidades" onClick={handleLinkClick}>
+              {lang === "es" ? "Habilidades" : "Skills"}
+            </a>
+            <a href="#proyectos" onClick={handleLinkClick}>
+              {lang === "es" ? "Proyectos" : "Projects"}
+            </a>
+            <a href="#contacto" onClick={handleLinkClick}>
+              {lang === "es" ? "Contacto" : "Contact"}
+            </a>
+        </nav>
+        {/* Pruebas cambios random (Eliminar) */}
 
         <div className="lang-switch">
           <div className="tabs">
